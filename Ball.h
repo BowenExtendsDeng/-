9e2469d9155caeb0828140a213e5a72231fdc6f2
邓博文 <2631702650@qq.com>
2022-05-20 10:07:17 +0800

    补充了一些必要的注释

--- Ball.h
+++ Ball.h
@@ -5,9 +5,11 @@
 class Ball
 {
 private:
+	// 定义球的速度区间
 	const int kMaxSpeed = 30;
 	const int kMinSpeed = 10;
 
+	// 球的基本属性：速度，位置，颜色，半径
 	int speed_x;
 	int speed_y;
 	int pos_x;
@@ -17,35 +19,41 @@ private:
 	int color_b;
 	int radius;
 
+	// 定义球的方向，规定右下为正，左上为负
 	int direction_x = 1;
 	int direction_y = 1;
 
 public:
+	// 提供有且仅有一种构造i函数
 	Ball(int init_speed_x, int init_speed_y, int init_pos_x, int init_pos_y, 
 		int init_color_r, int init_color_g, int init_color_b, int init_radius);
 
+	// 绘制球的图案
 	void Draw();
 
+	// 移动球的横坐标
 	void MovePosX();
 
+	// 移动球的纵坐标
 	void MovePosY();
 
+	// 让球x,y反向，并重新设置速度
 	void AbsAll();
 
+	// 改变球的方向
 	void DirectionUp();
-
 	void DirectionDown();
-
 	void DirectionLeft();
-
 	void DirectionRight();
 
+	// 重新给速度赋值，速度在上面定义的速度区间内
 	void RandSpeedX();
-
 	void RandSpeedY();
 
+	// 获取圆心横坐标
 	int GetPosX();
 
+	//获取圆心纵坐标
 	int GetPosY();
 };
 
--- main.cpp
+++ main.cpp
@@ -8,23 +8,34 @@ using namespace std;
 
 int main()
 {
+	// 设置窗口大小
 	const int kGraphWitdh = 800;
 	const int kGraphHeight = 800;
 	
+	// 定义两球半径
 	const int kRadius1 = 30;
 	const int kRadius2 = 45;
 
+	/*
+	 * 碰撞锁：因为两球相撞速度只改变一次，不加锁可能导致两球重复碰撞
+	 * 加锁：两球碰撞时加锁，因为碰撞后两球到撞墙前不可能再相遇
+	 * 解锁：球碰墙时解锁，因为反向后有重新碰撞的可能
+	 */
 	bool collision_lock = false;
 
+	// 初始化两个球
 	Ball *circle1 = new Ball(20, 20, 50, 80, 240, 128, 128, kRadius1);
 	Ball *circle2 = new Ball(30, 15, 600, 400, 64, 224, 208, kRadius2);
     
-
+	// 绘制界面
 	initgraph(kGraphWitdh, kGraphHeight);
 	setbkcolor(WHITE);
 	cleardevice();
 
+	// 进入事件循环
 	while (true) {
+		
+		// 球碰墙检测
 		if (kGraphWitdh <= circle1->GetPosX() + kRadius1) {
 			circle1->DirectionLeft();
 			collision_lock = false;
@@ -58,6 +69,7 @@ int main()
 			collision_lock = false;
 		}
 
+		//两球相撞检测
 		bool balls_touched = ((kRadius1 + kRadius2) >= sqrt(pow(circle1->GetPosX()
 			- circle2->GetPosX(), 2) + pow(circle1->GetPosY() - circle2->GetPosY(), 2)));
 		if (!collision_lock && balls_touched) {
@@ -66,6 +78,7 @@ int main()
 			collision_lock = true;
 		}
 
+		//绘制图形
 		circle1->Draw();
 		circle2->Draw();
 		Sleep(1);
